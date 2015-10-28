7e12e8b754ac3745ce8f2c8dd4f35c9533d3e764
ddiep003@ucr.edu <ddiep003@ucr.edu>
2015-10-28 16:22:53 -0700

    made parsing

--- src/main.cpp
+++ src/main.cpp
@@ -12,27 +12,150 @@
 #include <csignal>
 #include <queue>
 
+using namespace std;
+void display(string x)
+{
+    for (int i = 0; x[i] != '\0'; ++i)
+    {
+        cout << x[i];
+    }
+    cout << endl;
+}
+void parsing(string& cmd)
+{
+    char* parsed = (char*) malloc (50000);
+    for (int i = 0, j = 0; cmd[i] != '\0'; ++i, ++j)
+    {
+        if(cmd[i] == '#')
+        {
+            cout << "made it#" << endl;
+            cmd[i] = '\0';
+            parsed[j] = '\0';
+        }
+        else if (cmd[i] == ';')
+        {
+            cout << "made it;" << endl;
+            parsed[j] = ' ';
+            parsed[++j] = ';';
+            parsed[++j] = ' ';
+            display(parsed);
+        }
+        else if (cmd[i] == '|' && cmd[i + 1] == '|')
+        {
+            cout << "made it||" << endl;
+            parsed[j] = ' ';
+            parsed[++j] = '|';
+            parsed[++j] = '|';
+            parsed[++j] = ' ';
+            ++i;
+        }
+        else if (cmd[i] == '&' && cmd[i + 1] == '&')//&& connector
+        {
+            cout << "made it&&" << endl;
+            parsed[j] = ' ';
+            parsed[++j] = '&';
+            parsed[++j] = '&';
+            parsed[++j] = ' ';
+            ++i;
+        }
+        else if (cmd[i] == '>' && cmd[i + 1] == '>')//stdout redirection append
+        {
+            parsed[j] = ' ';
+            parsed[++j] = '>';
+            parsed[++j] = '>';
+            parsed[++j] = ' ';
+            ++i;
+        }
+        else if (cmd[i] == '>')//stdout redirection
+        {
+			parsed[j] = ' ';
+			parsed[++j] = '>';
+			parsed[++j] = ' ';
 
+        }
+        else if (cmd[i] == '<')//stdin redirection
+        {
+			parsed[j] = ' ';
+			parsed[++j] = '<';
+			parsed[++j] = ' ';
+        }
+        else if ((cmd[i] == '0' || cmd[i] == '1' || cmd[i] == '2') &&
+				cmd[i + 1] == '>')
+        {
+            if (cmd[i] == '0')
+            {
+                parsed[j] = '0';
+            }
+            else if (cmd[i] == '1') 
+            {
+                parsed[j] = '1';
+            }
+            else if (cmd[i] == '2') 
+            {
+                parsed[j] = '2';
+                parsed[++j] = '>';
+            }
+            if (cmd[i + 2] == '>')
+            {
+				++i;
+				parsed[++j] = '>';
+            }
+            ++i;
+        }
+        else
+        {
+            //cout <<"This is parsed at " << parsed[j] << endl;
+            parsed[j] = cmd[i];
+        }
+        if (cmd[i + 1] == '\0')
+        {
+            parsed[j + 1] = '\0';
+            //cout << "Index of j: " << j << end
+        }
+        display(parsed);
+    }
+    cout << "Original " << cmd << endl;
+    cout << parsed <<endl;
+    display(parsed);
+    cmd = parsed;
+    cout << "new cmd " << cmd << endl;
+    //const char *c = cmd.c_str();
+    //strcpy(, parsed);
+    //free(parsed)
+}
+
+void myFork()
+{
+	pid_t pid = fork();
+	if(pid < 0)
+	{
+		cout << "Fork Failed" << endl;
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		cout << "Child: I'm the child: " << pid << endl;
+	}
+	else if (pid > 0)
+	{
+		cout << "Parent: I'm the parent: " << pid << endl;
+	}
+}
 int main (int argc, char **argv)
 {
-    //std::cout << "Hello";
-    pid_t pid; // integer that can store a process ID (use pid_t for portability)
+    string cmd;
+    //while (cmd != "exit")
+    //{
+        cout << "$ ";
+        
+        getline(cin, cmd);
+        //cout << cmd << endl;
+    //}
 
-    if ((pid = fork()) == -1) // system functions also set a variable called "errno"
-    {
-        perror("fork"); // this function automatically checks "errno"
-        // and prints the error plus what you give it
-        return EXIT_FAILURE;
-    }
-// ---- by when you get here there will be two processes
-    if (pid == 0) // child process
-    {
-        // look up exec functions
-    }
-    else // parent process
-    {
-    }
+    parsing(cmd);
+    cout << cmd << endl;;
+    
 
-    return EXIT_SUCCESS;
-    return 0;
+    
+    
 }
